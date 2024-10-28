4e3eb4fa553680ace7933fb81eb9ec730bd9f004
Dominic <domebingonye@gmail.com>
2024-10-28 20:42:06 +0200

    initial commit

---
+++ src/main/java/com/mildtech_solution/simple_crud_services/SimpleCrudServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mildtech_solution.simple_crud_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mildtech_solution/simple_crud_services/SimpleCrudServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mildtech_solution.simple_crud_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
